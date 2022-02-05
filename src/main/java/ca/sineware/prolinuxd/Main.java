c68c5b7290fb6096097d495a42de435d120aa64d
Seshan Ravikumar <seshan10@me.com>
2022-02-05 00:17:30 -0500

    Switch licence to LGPL 2.1
    Lesser restrictions for integrating ProLinuxd into products

--- src/main/java/ca/sineware/prolinuxd/Main.java
+++ src/main/java/ca/sineware/prolinuxd/Main.java
@@ -2,18 +2,8 @@
     Sineware Cloud Services Client Daemon (prolinuxd)
     Copyright (C) 2022 Seshan Ravikumar
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    This software is licensed under the GNU Lesser General Public License
+    (version 2.1 or later). See the LICENSE file in this distribution.
  */
 package ca.sineware.prolinuxd;
 import ca.sineware.prolinuxd.dbus.DbusClient;
